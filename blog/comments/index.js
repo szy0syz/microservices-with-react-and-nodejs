8be60b34cdbb75cb04035c748142a43259e82759
Jerry <szy0syz@gmail.com>
2021-08-12 17:22:27 +0800

    feat: client & comment service

---
+++ blog/comments/index.js
@@ -0,0 +1,29 @@
+const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log('Comments Service listening on 4001');
+});
