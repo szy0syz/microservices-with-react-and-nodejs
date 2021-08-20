58153d1827a71794d455e4493e6e13f61f7165e1
szy0syz <szy0syz@gmail.com>
2021-08-21 00:14:46 +0800

    feat: k8s中安装MongoDB和Express中连接

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { json } from 'body-parser';
 import 'express-async-errors';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -22,6 +24,22 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    console.log('Connected to Mongodb!');
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!');
+  });
+};
+
+start();
