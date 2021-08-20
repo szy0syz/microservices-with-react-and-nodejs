052761f83025641d9a0ce4d51050c02328dbf72d
szy0syz <szy0syz@gmail.com>
2021-08-20 11:15:47 +0800

    feat: auth-signup & test

---
+++ ticketing/auth/src/routes/signup.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+
+const router = express.Router();
+
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+    const { email, password } = req.body;
+
+    console.log("Creating a user...");
+
+    res.send({});
+
+    // new User({ email, password })
+  }
+);
+
+export { router as signupRouter };
