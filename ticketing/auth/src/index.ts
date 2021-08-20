33edb85f16aebf3b9e1e644ee5f42bfd6248519a
szy0syz <szy0syz@gmail.com>
2021-08-20 23:45:55 +0800

    feat: express-async-error

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
-import { json } from "body-parser";
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
+import express from 'express';
+import { json } from 'body-parser';
+import 'express-async-errors';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -16,12 +16,12 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all("*", () => {
+app.all('*', async () => {
   throw new NotFoundError();
 });
 
 app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000!!!!!!!!");
+  console.log('Listening on port 3000!');
 });
--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -14,14 +14,14 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
     console.log("Creating a user...");
-    throw new DatabaseConnectionError();
+    // throw new DatabaseConnectionError();
 
     res.send({});
   }
