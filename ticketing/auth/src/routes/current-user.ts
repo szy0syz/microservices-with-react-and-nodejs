e171ec57013e7d556162d9d3941e1c4163d9dff9
szy0syz <szy0syz@gmail.com>
2021-08-23 23:14:20 +0800

    feat: 完成中间件制作

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,19 +1,11 @@
+import { requireAuth } from './../middlewares/require-auth';
+import { currentUser } from './../middlewares/current-user';
 import express from 'express';
-import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (err) {
-    res.send({ currentUser: null });
-  }
+router.get('/api/users/currentuser', requireAuth, currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
