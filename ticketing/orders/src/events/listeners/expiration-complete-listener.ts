5c3139f21abf0a5420d16a26fcc13b7b724c78e3
szy0syz <szy0syz@gmail.com>
2021-09-09 23:04:27 +0800

    feat: Don't Cancel Completed Orders

--- ticketing/orders/src/events/listeners/expiration-complete-listener.ts
+++ ticketing/orders/src/events/listeners/expiration-complete-listener.ts
@@ -20,6 +20,10 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
       throw new Error('Order not found');
     }
 
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
+    }
+
     //order.set({ ticket: null, status: OrderStatus.Cancelled });
     order.set({ status: OrderStatus.Cancelled });
     await order.save();
