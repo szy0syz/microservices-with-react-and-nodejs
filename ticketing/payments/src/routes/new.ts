39c615987d41c74ed31a35c2eab0c776da801e2b
szy0syz <szy0syz@gmail.com>
2021-09-09 09:25:31 +0800

    feat: stripe

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -8,6 +8,7 @@ import {
   NotFoundError,
   OrderStatus,
 } from '@js-ticketing/common';
+import { stripe } from '../stripe';
 import { Order } from '../models/order';
 
 const router = express.Router();
@@ -33,6 +34,12 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for an cancelled order');
     }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token,
+    });
 
     res.send({ success: true });
   }
