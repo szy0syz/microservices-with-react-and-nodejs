0f904dfa006307004b526c53b9bc100dba0b14d3
szy0syz <szy0syz@gmail.com>
2021-09-10 00:23:56 +0800

    feat: final Client

--- ticketing/client/components/header.js
+++ ticketing/client/components/header.js
@@ -1,12 +1,14 @@
 import Link from 'next/link';
 
-const Header =  ({ currentUser }) => {
+export default ({ currentUser }) => {
   const links = [
     !currentUser && { label: 'Sign Up', href: '/auth/signup' },
     !currentUser && { label: 'Sign In', href: '/auth/signin' },
-    currentUser && { label: 'Sign Out', href: '/auth/signout' }
+    currentUser && { label: 'Sell Tickets', href: '/tickets/new' },
+    currentUser && { label: 'My Orders', href: '/orders' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' },
   ]
-    .filter(linkConfig => linkConfig)
+    .filter((linkConfig) => linkConfig)
     .map(({ label, href }) => {
       return (
         <li key={href} className="nav-item">
@@ -29,5 +31,3 @@ const Header =  ({ currentUser }) => {
     </nav>
   );
 };
-
-export default Header;
--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,18 +1,39 @@
-const LandingPage = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are signed in</h1>
-  ) : (
-    <h1>You are NOT signed in</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            <a>View</a>
+          </Link>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
-// LandingPage.getInitialProps = async (context) => {
-  // 这里因为在 AppContainer 中已经获取一次了，我们及传递下来，不要二次重复
-  // const client = buildClient(context);
-  // const { data } = await client
-  //   .get('/api/users/currentuser')
-  //   .catch((err) => console.log('报错了', err));
-  // return data;
-// };
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data: tickets } = await client.get('/api/tickets');
+
+  return { tickets };
+};
 
 export default LandingPage;
